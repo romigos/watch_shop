6e6cceeaa4e97d66c6846308547e19c6f9a94e4e
romigos <bolist@mail.ru>
2020-03-15 09:53:25 -0700

    add two tests

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,16 @@
+# frozen_string_literal: true
+
 class MainController < ApplicationController
+  before_action :set_page_options
+
   def index
     @brands = Brand.limit(3)
     @hits = Product.all.limit(8)
+  end
+
+  def set_page_options
+    @page_title = 'Watchers Shop'
+    @page_description = 'Watchers'
+    @page_keywords = 'Men, Woman, Kids'
   end
 end
